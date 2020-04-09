66d7769b54cf988da5c4d12e62dd15eb67c6f95e
Nikolas Huebecker <nikolas.huebecker@gmail.com>
2020-04-09 15:12:17 -0400

    I can't spell

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default () => (
           </ul>
         </div>
         <div className="pt-8 leading-9">
-          <p><strong>Nikolas Huebecker</strong> is a Knowledge Sorceror at <a href="/shh"><span className="redacted">*********</span></a>, a "Stealth Startup" building a <span className="redacted">******* ****</span> to help both individuals and organizations <span className="redacted">*****</span> better.</p>
+          <p><strong>Nikolas Huebecker</strong> is a Knowledge Sorcerer at <a href="/shh"><span className="redacted">*********</span></a>, a "Stealth Startup" building a s<span className="redacted">***** ** ****</span> to help both individuals and organizations <span className="redacted">***** *****</span>r.</p>
           <p className="my-5">In middle school he helped start <a href="https://www.producthunt.com/posts/ease-2">EaseSMS</a>, a SMS based hybrid assistant. During his freshman year, he joined <a href="https://web.archive.org/web/20180821062406/https://stersvoice.com/#WhatWeDo">TeenMade (Later StersVoice)</a>, a Gen Z focused Digital Agency, as the COO.</p>
           <p className="my-5">Post-StersVoice, he interned at <a href="https://shrug.vc">Shrug Capital</a> where he helped a number of startups, like <a href="https://panda.af">Panda</a> shape their Gen Z focused products.</p>
         </div>
