0ba95b887c1fac54769a6131889108a01ae395c5
nhuebecker <nikolas.huebecker@gmail.com>
2020-08-10 21:27:46 -0700

    Fix bio

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,44 @@
-import Head from 'next/head'
-import Link from 'next/link'
+import Head from "next/head";
+import Link from "next/link";
 
-import Wrapper from '../components/Wrapper'
-import Nav from '../components/Nav'
+import Wrapper from "../components/Wrapper";
+import Nav from "../components/Nav";
 
 export default () => (
   <Wrapper title="Nikolas Huebecker | Blog">
     <Nav />
     <div className="pt-8">
-      <p className="mb-4"><strong>Nikolas Huebecker</strong> is a Knowledge Sorcerer at <a href="/shh"><span className="redacted">*********</span></a>, a "Stealth Startup" building a s<span className="redacted">***** ** ****</span> to help both individuals and organizations <span className="redacted">***** *****</span>r.</p>
-      <p className="my-4">For the past 4 years, Nikolas has worked with <a href="https://codeday.org">SRND</a> the non-profit behind <a href="https://codeday.org">CodeDay</a>, a worldwide event where programmers, artists, musicians and everyone else can get together and build apps & games for 24 hours</p>
-      <p className="my-4">In middle school he helped start <a href="https://www.producthunt.com/posts/ease-2">EaseSMS</a>, a SMS based hybrid assistant. During his freshman year, he joined <a href="https://web.archive.org/web/20180821062406/https://stersvoice.com/#WhatWeDo">TeenMade (Later StersVoice)</a>, a Gen Z focused Digital Agency, as the COO.</p>
-      <p className="my-4">Post-StersVoice, he interned at <a href="https://shrug.vc">Shrug Capital</a> where he helped a number of startups, like <a href="https://panda.af">Panda</a> shape their Gen Z focused products.</p>
+      <p className="mb-4">
+        <strong>Nikolas Huebecker</strong> is working on{" "}
+        <a href="/shh">
+          <span className="">[REDACTED]</span>
+        </a>
+        , building a s<span className="redacted">***** ** ****</span> to help
+        both individuals and organizations{" "}
+        <span className="redacted">***** *****</span>r.
+      </p>
+      <p className="my-4">
+        For the past 4 years, Nikolas has worked with{" "}
+        <a href="https://codeday.org">SRND</a> the non-profit behind{" "}
+        <a href="https://codeday.org">CodeDay</a>, a worldwide event where
+        programmers, artists, musicians and everyone else can get together and
+        build apps & games for 24 hours
+      </p>
+      <p className="my-4">
+        In middle school he helped start{" "}
+        <a href="https://www.producthunt.com/posts/ease-2">EaseSMS</a>, a SMS
+        based hybrid assistant. During his freshman year, he joined{" "}
+        <a href="https://web.archive.org/web/20180821062406/https://stersvoice.com/#WhatWeDo">
+          TeenMade (Later StersVoice)
+        </a>
+        , a Gen Z focused Digital Agency, as the COO.
+      </p>
+      <p className="my-4">
+        Post-StersVoice, he interned at{" "}
+        <a href="https://shrug.vc">Shrug Capital</a> where he helped a number of
+        startups, like <a href="https://panda.af">Panda</a> shape their Gen Z
+        focused products.
+      </p>
     </div>
     <div className="mt-8 mb-12 text-center">
       <ul className="flex justify-center align-center">
@@ -24,4 +51,4 @@ export default () => (
       </ul>
     </div>
   </Wrapper>
-)
+);
